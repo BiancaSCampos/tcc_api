703bb72990149c9e4cac1405a09affcbeb4477b8
bianca da silva campos <biancacampos@1sti.com.br>
2023-05-16 18:29:04 -0300

    removing comments

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,21 +6,17 @@ dotenv.config();
 
 const router = express.Router();
 
-// Criar um novo recurso
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
-  // Verificar se o usuário existe e se a senha está correta
   const user = await verifyUser(username, password);
 
   if (!user) {
     return res.status(401).json({ message: "Credenciais inválidas" });
   }
 
-  // Gerar um token JWT para o usuário
   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
 
-  // Retornar o token JWT para o cliente
   res.json({ token });
 });
 
