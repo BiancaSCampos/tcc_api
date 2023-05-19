40f1321113d4ec0edc9cf7104003094452a502db
bianca da silva campos <biancacampos@1sti.com.br>
2023-05-19 13:27:09 -0300

    atendimento and sinalizações routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import medicoRouter from "./routes/medico.js";
 import pacienteRouter from "./routes/paciente.js";
 import authRouter from "./routes/auth.js";
+import atendimentoRouter from "./routes/atendimento.js";
+import sinalizacaoRouter from "./routes/sinalizacoes.js";
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.use("/medicos", medicoRouter);
 app.use("/pacientes", pacienteRouter);
 app.use("/login", authRouter);
+app.use("/atendimento", atendimentoRouter);
+app.use("/sinalizacoes", sinalizacaoRouter);
 
 app.listen(3089, () => {
   console.log("Servidor iniciado na porta 3089");
