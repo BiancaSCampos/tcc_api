363eb9ab0c35f075d4a8d03429c3c9396729fe21
bianca da silva campos <biancacampos@1sti.com.br>
2023-05-19 18:36:34 -0300

    exame model

--- src/models/exame.js
+++ src/models/exame.js
@@ -18,6 +18,23 @@ export function findAll(callback) {
   });
 }
 
+export function findByPatient(id) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      `SELECT * FROM exame WHERE paciente_idpaciente = ?`,
+      [id],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+}
+
 function atualizar(id, exame, callback) {
   connection.query("UPDATE exame SET ? WHERE id = ?", [exame, id], callback);
 }
