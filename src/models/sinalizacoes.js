c83f47decc6074b4f75ab36e49fc3ef2fee3a38d
bianca da silva campos <biancacampos@1sti.com.br>
2023-05-19 18:36:57 -0300

    sinalizações model

--- src/models/sinalizacoes.js
+++ src/models/sinalizacoes.js
@@ -1,23 +1,5 @@
 import { connection } from "../config/dbconfig.js";
 
-function criar(paciente, callback) {
-  connection.query("INSERT INTO paciente SET ?", paciente, callback);
-}
-
-function findByID(id, callback) {
-  connection.query("SELECT * FROM paciente WHERE id = ?", (err, result) => {
-    if (err) throw err;
-    callback(result);
-  });
-}
-
-export function findAll(callback) {
-  connection.query("SELECT * FROM paciente", (err, result) => {
-    if (err) throw err;
-    callback(result);
-  });
-}
-
 export function findByPatient(id) {
   return new Promise((resolve, reject) => {
     connection.query(
@@ -33,16 +15,4 @@ export function findByPatient(id) {
       }
     );
   });
-}
-
-function atualizar(id, paciente, callback) {
-  connection.query(
-    "UPDATE paciente SET ? WHERE id = ?",
-    [paciente, id],
-    callback
-  );
-}
-
-function excluir(id, callback) {
-  connection.query("DELETE FROM paciente WHERE id = ?", id, callback);
 }
